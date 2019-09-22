5e7a527cee1a5f931a411db70757444bdad5264d
Johnson <zxmagichacker@gmail.com>
2019-09-22 10:30:48 +0800

    feat: 构建明细页

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,6 +1,29 @@
 //Page Object
 Page({
-  data: {},
+  data: {
+    detailList: [
+      {
+        date: "06月09日星期六",
+        hour: 8,
+        itemList: [{ title: "沟通需求" }]
+      },
+      {
+        date: "06月08日星期五",
+        hour: 8,
+        itemList: [
+          { title: "原型-首页模块" },
+          { title: "原型-首页模块" },
+          { title: "原型-首页模块" }
+        ]
+      },
+      {
+        date: "06月07日星期四",
+        hour: 8,
+        itemList: [{ title: "原型-首页模块" }, { title: "原型-首页模块" }]
+      }
+    ],
+    date: "2019-09-09"
+  },
   //options(Object)
   onLoad: function(options) {},
   onReady: function() {},
@@ -12,5 +35,11 @@ Page({
   onShareAppMessage: function() {},
   onPageScroll: function() {},
   //item(index,pagePath,text)
-  onTabItemTap: function(item) {}
+  onTabItemTap: function(item) {},
+  // 时间选择
+  changeDate(e) {
+    this.setData({
+      date: e.detail.value
+    });
+  }
 });
