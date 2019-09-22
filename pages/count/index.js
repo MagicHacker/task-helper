c8e4efd57b6cf82383ea7218dd94f784f01f2501
Johnson <zxmagichacker@gmail.com>
2019-09-22 16:25:49 +0800

    feat: 构建统计页面

--- pages/count/index.js
+++ pages/count/index.js
@@ -1,10 +1,38 @@
-//Page Object
+const wxCharts = require("../../utils/wxcharts-min.js");
 Page({
   data: {},
   //options(Object)
   onLoad: function(options) {},
   onReady: function() {},
-  onShow: function() {},
+  onShow: function() {
+    new wxCharts({
+      canvasId: "count-hours",
+      type: "line",
+      width: 350,
+      height: 250,
+      dataPointShape: "circle",
+      dataLabel: true,
+      legend: true,
+      categories: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
+      series: [
+        {
+          name: "预计耗时",
+          data: [4, 7, 6, 4, 3, 10, 6]
+        },
+        {
+          name: "实际耗时",
+          data: [6, 9, 8, 6, 5, 12, 8]
+        }
+      ],
+      xAxis: {
+        disableGrid: false
+      },
+      yAxis: {
+        title: "小时",
+        min: 0
+      }
+    });
+  },
   onHide: function() {},
   onUnload: function() {},
   onPullDownRefresh: function() {},
