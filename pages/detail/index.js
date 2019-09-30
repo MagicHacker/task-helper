d58f11f28c5130bd760916df5b6df9b0a4781016
Johnson <zxmagichacker@gmail.com>
2019-09-30 21:33:42 +0800

    feat: 发请求动态构建明细页面

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,18 +1,34 @@
 //Page Object
+const api = require("../../api/index");
 Page({
   data: {
-    hour: 8,
-    itemList: [
-      { title: "原型-首页模块", hour: 8 },
-      { title: "原型-首页模块", hour: 8 },
-      { title: "原型-首页模块", hour: 8 }
-    ],
+    detailLists: [],
     date: "选择时间"
   },
   //options(Object)
   onLoad: function(options) {},
   onReady: function() {},
-  onShow: function() {},
+  onShow: function() {
+    api
+      .request("http://localhost:8080/detail/getLists", {}, "GET")
+      .then(res => {
+        const { detailLists } = res.data;
+        const hour = detailLists.map(val => {
+          return val.lists.reduce((total, currentVal) => {
+            return (
+              (total.actualHours ? total.actualHours : total) +
+              currentVal.actualHours
+            );
+          });
+        });
+        const lists = detailLists.map((item, index) => {
+          return Object.assign({}, item, { hour: hour[index] });
+        });
+        this.setData({
+          detailLists: lists
+        });
+      });
+  },
   onHide: function() {},
   onUnload: function() {},
   onPullDownRefresh: function() {},
