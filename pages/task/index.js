c756820dc75bd2c5889ab38cdd844a8f2bc3ca1b
Johnson <zxmagichacker@gmail.com>
2019-10-06 01:01:08 +0800

    feat: 查询任务列表

--- pages/task/index.js
+++ pages/task/index.js
@@ -2,17 +2,26 @@
 const api = require("../../api/index");
 Page({
   data: {
-    taskList: []
+    taskList: [],
+    taskId: 0
   },
   //options(Object)
   onLoad: function(options) {},
   onReady: function() {},
   onShow: function() {
-    api.request("http://localhost:8080/task/getLists", {}, "GET").then(res => {
-      this.setData({
-        taskList: res.data.taskLists
+    const userId = wx.getStorageSync("userId");
+    api
+      .request("http://localhost:8080/task/getLists", { userId }, "GET")
+      .then(res => {
+        res.data.forEach(item => {
+          this.setData({
+            taskId: item.id
+          });
+        });
+        this.setData({
+          taskList: res.data
+        });
       });
-    });
   },
   onHide: function() {},
   onUnload: function() {},
@@ -22,9 +31,10 @@ Page({
   onPageScroll: function() {},
   onTabItemTap: function(item) {},
   // 跳转到详情页
-  navigateToDetail() {
+  navigateToDetail(event) {
+    const { id } = event.currentTarget.dataset;
     wx.navigateTo({
-      url: "/pages/task-detail/index"
+      url: "/pages/task-detail/index?id=" + id
     });
   },
   // 添加任务
