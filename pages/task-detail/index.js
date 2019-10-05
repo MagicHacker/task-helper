e93004875103b7b8aac9b905e1e56f93732453d6
Johnson <zxmagichacker@gmail.com>
2019-10-06 01:01:40 +0800

    feat: 编辑，保存，删除任务详情

--- pages/task-detail/index.js
+++ pages/task-detail/index.js
@@ -1,13 +1,41 @@
 //Page Object
+const api = require("../../api/index");
+const baseURI = require("../../api/base");
 Page({
   data: {
     startDate: "请选择",
-    endDate: "请选择"
+    endDate: "请选择",
+    taskId: 0,
+    taskName: "",
+    taskType: "",
+    taskHour: 0,
+    actualHour: 0,
+    taskDes: "",
+    id: 0
   },
   //options(Object)
-  onLoad: function(options) {},
+  onLoad: function(options) {
+    this.setData({
+      taskId: options.id
+    });
+  },
   onReady: function() {},
-  onShow: function() {},
+  onShow: function() {
+    api
+      .request(baseURI + "/task/getDetails", { id: this.data.taskId }, "GET")
+      .then(res => {
+        this.setData({
+          taskName: res.data[0].task_name,
+          startDate: res.data[0].start_date,
+          endDate: res.data[0].end_date,
+          taskType: res.data[0].task_type,
+          taskHour: res.data[0].task_hour,
+          actualHour: res.data[0].actual_hour,
+          taskDes: res.data[0].task_des,
+          id: res.data[0].id
+        });
+      });
+  },
   onHide: function() {},
   onUnload: function() {},
   onPullDownRefresh: function() {},
@@ -28,25 +56,74 @@ Page({
       endDate: event.detail.value
     });
   },
+  // 获取任务名称
+  getTaskName(event) {
+    this.setData({
+      taskName: event.detail.value
+    });
+  },
+  // 获取任务类型
+  getTaskType(event) {
+    this.setData({
+      taskType: event.detail.value
+    });
+  },
+  // 获取预计耗时
+  getTaskHour(event) {
+    this.setData({
+      taskHour: event.detail.value
+    });
+  },
+  // 获取实际耗时
+  getActualHour(event) {
+    this.setData({
+      actualHour: event.detail.value
+    });
+  },
+  // 获取任务描述
+  getTaskDes(event) {
+    this.setData({
+      taskDes: event.detail.value
+    });
+  },
   // 保存任务
   saveTask() {
-    wx.showToast({
-      title: "保存成功",
-      icon: "success",
-      image: "",
-      duration: 1500,
-      mask: false,
-      success: result => {
-        wx.reLaunch({
-          url: "/pages/task/index",
-          success: result => {},
-          fail: () => {},
-          complete: () => {}
-        });
-      },
-      fail: () => {},
-      complete: () => {}
-    });
+    api
+      .request(
+        baseURI + "/task/changeDetails",
+        {
+          taskName: this.data.taskName,
+          taskType: this.data.taskType,
+          taskHour: this.data.taskHour,
+          taskDes: this.data.taskDescription,
+          id: this.data.id,
+          startDate: this.data.startDate,
+          endDate: this.data.endDate,
+          actualHour: this.data.actualHour
+        },
+        "POST"
+      )
+      .then(res => {
+        if (res.data) {
+          wx.showToast({
+            title: "保存成功",
+            icon: "success",
+            image: "",
+            duration: 1500,
+            mask: false,
+            success: result => {
+              wx.reLaunch({
+                url: "/pages/task/index",
+                success: result => {},
+                fail: () => {},
+                complete: () => {}
+              });
+            },
+            fail: () => {},
+            complete: () => {}
+          });
+        }
+      });
   },
   //删除任务
   deleteTask() {
@@ -60,20 +137,30 @@ Page({
       confirmColor: "#3CC51F",
       success: result => {
         if (result.confirm) {
-          wx.showToast({
-            title: "删除成功",
-            icon: "success",
-            image: "",
-            duration: 1500,
-            mask: false,
-            success: result => {
-              wx.reLaunch({
-                url: "/pages/task/index"
-              });
-            },
-            fail: () => {},
-            complete: () => {}
-          });
+          api
+            .request(
+              baseURI + "/task/delete/taskItem",
+              { id: this.data.id },
+              "DELETE"
+            )
+            .then(res => {
+              if (res.data) {
+                wx.showToast({
+                  title: "删除成功",
+                  icon: "success",
+                  image: "",
+                  duration: 1500,
+                  mask: false,
+                  success: result => {
+                    wx.reLaunch({
+                      url: "/pages/task/index"
+                    });
+                  },
+                  fail: () => {},
+                  complete: () => {}
+                });
+              }
+            });
         }
       },
       fail: () => {},
