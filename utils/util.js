73b1d8dc0df3616cbfdc74ecce496d1eb51f28fc
Johnson <zxmagichacker@gmail.com>
2019-10-07 23:39:36 +0800

    feat: 新增获取当前日期的方法

--- utils/util.js
+++ utils/util.js
@@ -21,7 +21,18 @@ const formatNumber = n => {
 const createUUid = () => {
   return Math.floor(Math.random() * 100000 + 1);
 };
+// 获取当前时间
+const currentDate = () => {
+  const date = new Date();
+  const month =
+    date.getMonth() >= 0 && date.getMonth() <= 8
+      ? "0" + (date.getMonth() + 1)
+      : date.getMonth() + 1;
+  const day = date.getDate() < 9 ? "0" + date.getDate() : date.getDate();
+  return `${date.getFullYear()}-${month}-${day}`;
+};
 module.exports = {
   formatTime,
-  createUUid
+  createUUid,
+  currentDate
 };
