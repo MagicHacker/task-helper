4d3135bed82e69310ca0cecb76810babaaec9d39
Johnson <zxmagichacker@gmail.com>
2019-10-08 19:12:24 +0800

    feat: 统计页面查询数据

--- pages/count/index.js
+++ pages/count/index.js
@@ -1,13 +1,17 @@
 const wxCharts = require("../../utils/wxcharts-min.js");
+const api = require("../../api/index");
+const baseURI = require("../../api/base");
 Page({
   data: {
     date: "周",
-    timeDate: "上周"
+    timeDate: "上周",
+    itemList: []
   },
   //options(Object)
   onLoad: function(options) {},
   onReady: function() {},
   onShow: function() {
+    this.fetchData();
     new wxCharts({
       canvasId: "count-hours",
       type: "line",
@@ -54,5 +58,22 @@ Page({
     this.setData({
       timeDate: event.target.dataset.date
     });
+  },
+  // 拉取数据
+  fetchData() {
+    api
+      .request(
+        baseURI + "/count/getLists",
+        {
+          startDate: "2019-10-03",
+          userId: 39442
+        },
+        "GET"
+      )
+      .then(res => {
+        this.setData({
+          itemList: res.data.slice(0, 3)
+        });
+      });
   }
 });
