bb0b140e1a08ae6de43bfb7c1eb0410beac7292b
Johnson <zxmagichacker@gmail.com>
2019-10-08 22:17:08 +0800

    feat: '周'条件切换更改数据

--- pages/count/index.js
+++ pages/count/index.js
@@ -1,6 +1,7 @@
 const wxCharts = require("../../utils/wxcharts-min.js");
 const api = require("../../api/index");
 const baseURI = require("../../api/base");
+const util = require("../../utils/util");
 Page({
   data: {
     date: "周",
@@ -11,33 +12,8 @@ Page({
   onLoad: function(options) {},
   onReady: function() {},
   onShow: function() {
-    this.fetchData();
-    new wxCharts({
-      canvasId: "count-hours",
-      type: "line",
-      width: 350,
-      height: 250,
-      dataPointShape: "circle",
-      dataLabel: true,
-      legend: true,
-      categories: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
-      series: [
-        {
-          name: "预计耗时",
-          data: [4, 7, 6, 4, 3, 10, 6]
-        },
-        {
-          name: "实际耗时",
-          data: [6, 9, 8, 6, 5, 12, 8]
-        }
-      ],
-      xAxis: {
-        disableGrid: false
-      },
-      yAxis: {
-        title: "小时",
-        min: 0
-      }
+    this.fetchData(this.handleDate()).then(data => {
+      this.pintCharts(data);
     });
   },
   onHide: function() {},
@@ -54,26 +30,91 @@ Page({
       date: event.target.dataset.date
     });
   },
+  // TODO: 待优化
   chooseTime(event) {
     this.setData({
       timeDate: event.target.dataset.date
     });
+    this.fetchData(this.handleDate()).then(data => {
+      this.pintCharts(data);
+    });
+  },
+  // 绘制charts
+  pintCharts(data) {
+    let taskHour = [];
+    let actualHour = [];
+    data.forEach(item => {
+      taskHour.push(item.task_hour);
+      actualHour.push(item.actual_hour);
+    });
+    new wxCharts({
+      canvasId: "count-hours",
+      type: "line",
+      width: 350,
+      height: 250,
+      dataPointShape: "circle",
+      dataLabel: true,
+      legend: true,
+      categories: [
+        "周一",
+        "周二",
+        "周三",
+        "周四",
+        "周五",
+        "周六",
+        "周日"
+      ].slice(0, data.length),
+      series: [
+        {
+          name: "预计耗时",
+          data: taskHour
+        },
+        {
+          name: "实际耗时",
+          data: actualHour
+        }
+      ],
+      xAxis: {
+        disableGrid: false
+      },
+      yAxis: {
+        title: "小时",
+        min: 0
+      }
+    });
   },
   // 拉取数据
-  fetchData() {
-    api
-      .request(
-        baseURI + "/count/getLists",
-        {
-          startDate: "2019-10-03",
-          userId: 39442
-        },
-        "GET"
-      )
-      .then(res => {
-        this.setData({
-          itemList: res.data.slice(0, 3)
+  fetchData(date) {
+    return new Promise((resolve, reject) => {
+      api
+        .request(
+          baseURI + "/count/getLists",
+          {
+            startDate: date,
+            userId: 39442
+          },
+          "GET"
+        )
+        .then(res => {
+          this.setData({
+            itemList: res.data.slice(0, 3)
+          });
+          resolve(res.data);
         });
-      });
+    });
+  },
+  // 处理日期 TODO: 待优化
+  handleDate() {
+    const time = new Date(util.currentDate());
+    const month =
+      time.getMonth() >= 0 && time.getMonth() <= 8
+        ? "0" + (time.getMonth() + 1)
+        : time.getMonth() + 1;
+    const day = time.getDate() < 9 ? "0" + time.getDate() : time.getDate();
+    return this.data.timeDate === "本周"
+      ? util.currentDate()
+      : `${time.getFullYear()}-${month}-${
+          Number(day) - 7 < 9 ? "0" + (Number(day) - 7) : Number(day) - 7
+        }`;
   }
 });
